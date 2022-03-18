1102801ada322d2c2e8a4fb38fbb4c2dcfdbe0a0
v.muravskyi <v.muravskyi@gmail.com>
2022-03-19 00:27:36 +0200

    save

---
+++ src/test/java/com/selenium/pom/dataproviders/TestDataProvider.java
@@ -0,0 +1,14 @@
+package com.selenium.pom.dataproviders;
+
+import com.selenium.pom.objects.Product;
+import com.selenium.pom.utils.JacksonUtils;
+import org.testng.annotations.DataProvider;
+
+public class TestDataProvider {
+
+    @DataProvider(name = "getFeaturedProducts", parallel = true)
+    public Object[] getFeaturedProducts() {
+        return JacksonUtils.deserializeJsonToObject("products", Product[].class);
+    }
+
+}
--- src/test/java/com/selenium/pom/tests/AddToCartTest.java
+++ src/test/java/com/selenium/pom/tests/AddToCartTest.java
@@ -1,8 +1,10 @@
 package com.selenium.pom.tests;
 
 import com.selenium.pom.base.BaseTest;
+import com.selenium.pom.dataproviders.TestDataProvider;
 import com.selenium.pom.objects.Product;
 import com.selenium.pom.pages.CartPage;
+import com.selenium.pom.pages.HomePage;
 import com.selenium.pom.pages.StorePage;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
@@ -14,12 +16,27 @@ public class AddToCartTest extends BaseTest {
         Product product = new Product(1215);
 
         CartPage cartPage = new StorePage(getDriver())
-            .load()
-            .getProductThumbnail()
-            .clickAddToCartBtn(product.getName())
-            .clickViewCart();
+                .load()
+                .getProductThumbnail()
+                .clickAddToCartBtn(product.getName())
+                .clickViewCart();
         Assertions.assertThat(cartPage.getProductName())
-            .isEqualTo(product.getName());
+                .isEqualTo(product.getName());
     }
+
+    @Test(dataProvider = "getFeaturedProducts", dataProviderClass = TestDataProvider.class)
+    public void addToCartFeaturedProduct(Product product) {
+        String productName = "Blue Shoes";
+
+        CartPage cartPage = new HomePage(getDriver())
+                .load()
+                .getProductThumbnail()
+                .clickAddToCartBtn(product.getName())
+                .clickViewCart();
+
+        Assertions.assertThat(cartPage.getProductName())
+                .isEqualTo(product.getName());
+    }
+
 
 }
