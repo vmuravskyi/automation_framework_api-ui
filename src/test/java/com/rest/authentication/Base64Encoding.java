2ba8b1ab2e411b0f3e7afd5f2ae9865f9833cfc4
v.muravskyi <v.muravskyi@gmail.com>
2022-04-24 20:55:02 +0300

    save

---
+++ src/test/java/com/rest/authentication/Base64Encoding.java
@@ -0,0 +1,20 @@
+package com.rest.authentication;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import org.testng.annotations.Test;
+
+public class Base64Encoding {
+
+    @Test
+    public void base64encoding() {
+        String usernameColumnPassword = "volodymyr:password";
+        String encoded = Base64.getEncoder().encodeToString(usernameColumnPassword.getBytes(StandardCharsets.UTF_8));
+        System.out.println(encoded);
+
+        byte[] decode = Base64.getDecoder().decode(encoded.getBytes(StandardCharsets.UTF_8));
+        String decoded = new String(decode);
+        System.out.println(decoded);
+    }
+
+}
